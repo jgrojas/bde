60f84f280fb72bf234b1a25fc07c8f43bddf8a78
amontoyag <73204666+amontoyag@users.noreply.github.com>
2020-10-27 01:10:52 -0500

    Tabla nave
    Se realizó la creación de la tabla nave y su cargue de datos, adicionalmente se modificó el csv de tiponave con el contenido de los campos de categoría de trb y eslora.

--- SQL/bdecode.sql
+++ SQL/bdecode.sql
@@ -21,8 +21,30 @@ create table tiponave(
  );
 
 /*Cargue*/
-COPY tiponave(cod_tiponave, nom_tiponave)
+COPY tiponave(cod_tiponave, nom_tiponave,categoria_trb,categoria_eslora)
 FROM '/mnt/c/wamp64/www/bde/SQL/csv/tiponave.csv'
+DELIMITER ';'
+CSV HEADER;
+/*----------------------------------------------------------------------------*/
+
+/*----------------------------------------------------------------------------*/
+/*Creacion y cargue de tabla nave
+/*----------------------------------------------------------------------------*/
+create table nave(
+ omimatricu char(20) primary key,
+ nombrenave char(40) not null,
+ codigo_pais char(6) not null,
+ id_agencia_arribo char(20) not null,
+ codigotiponave int,
+ anoconstru char(4),
+ trb numeric,
+ dwt numeric,
+ eslora numeric
+ );
+
+/*Cargue*/
+COPY nave(omimatricu, nombrenave,codigo_pais,id_agencia_arribo,codigotiponave,anoconstru,trb,dwt,eslora)
+FROM '/mnt/c/wamp64/www/bde/SQL/csv/nave.csv'
 DELIMITER ';'
 CSV HEADER;
 /*----------------------------------------------------------------------------*/
