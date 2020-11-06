b798f6b098f89fd02195aa67faaea7f3d703dad0
Juan Gabriel Rojas <gabriel.jugaro48@gmail.com>
2020-11-05 20:00:28 -0500

    Some changes

--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
 
@@ -9,6 +10,16 @@ class home extends Controller
 {
     public function index()
     {
+
+    	$arribos_capitanias=DB::TABLE('capitanias')
+    						->join('arribos_naves_puertos','arribos_naves_puertos.id_capitania','=','capitanias.id_capitania')
+    						->select(DB::RAW('nom_capitania, count(nom_capitania) as total'))
+    						->groupby('nom_capitania')
+    						->orderby('total','DESC')
+    						->get();
+
+    	return $arribos_capitanias;
+    		
         return view('pages.home');
     } 
 }
