a79095f3ce5e88592eb08a2ccae4c5f6547c84d6
amontoyag <73204666+amontoyag@users.noreply.github.com>
2020-11-21 21:58:33 -0500

    nuevas consultas php

--- SQL/conexion_bd/windows/1_ParametrosConexion_G2.bat
+++ SQL/conexion_bd/windows/1_ParametrosConexion_G2.bat
@@ -1,10 +1,10 @@
 REM Por favor suministre sus datos de conexion aqui
 set PGPORT=5432
-set PGHOST=172.18.144.242
-set PGUSER=postgres
-set SIMARDB=simarprueba
-set PGBIN=C:\Program Files\PostgreSQL\12\bin
-set PGPASSWORD=postgres
+set PGHOST=your_host_address
+set PGUSER=your_username
+set SIMARDB=your_database
+set PGBIN=path_to_pgsql.exe
+set PGPASSWORD=your_password
 
 REM cd to path of the shell script
 cd /d %~dp0
--- app/Http/Controllers/home.php
+++ app/Http/Controllers/home.php
@@ -14,7 +14,6 @@ class home extends Controller
                             ->select(DB::RAW('count(id_capitania) as total'))
                             ->get();
 
-
     	$arribos_capitanias=DB::TABLE('capitanias')
     						->join('arribos_naves_puertos','arribos_naves_puertos.id_capitania','=','capitanias.id_capitania')
     						->select(DB::RAW('nom_capitania, count(nom_capitania) as total'))
@@ -31,8 +30,35 @@ class home extends Controller
                             ->orderby('total','DESC') 
                             ->limit(10) 
                             ->get();
-        return $principales_zarpes;
-    		
+
+        $tipos_naves=DB::TABLE('tiponave')
+                            ->join('nave','nave.codigotiponave','=','tiponave.cod_tiponave')
+                            ->join('arribos_naves_puertos','arribos_naves_puertos.omimatricula','=','nave.omimatricula')
+                            ->select(DB::RAW('tiponave.nom_tiponave,count(tiponave.nom_tiponave) as total'))
+                            ->groupby('tiponave.nom_tiponave')
+                            ->orderby('total','DESC')
+                            ->get();
+
+        $arribos_capitanias2020=DB::TABLE('capitanias')
+                            ->whereBetween('arribos_naves_puertos.fecha_arribo',['2020-01-01','2020-11-21'])
+                            ->join('arribos_naves_puertos','arribos_naves_puertos.id_capitania','=','capitanias.id_capitania')
+                            ->select(DB::RAW('nom_capitania, count(nom_capitania) as total'))
+                            ->groupby('nom_capitania')
+                            ->orderby('total','DESC')
+                            ->limit(5)
+                            ->get(); 
+
+        $num_naves2020=DB::TABLE('arribos_naves_puertos')
+                            ->whereBetween('arribos_naves_puertos.fecha_arribo',['2020-01-01','2020-11-21'])
+                            ->select(DB::RAW('count(id_capitania) as total'))
+                            ->get();  
+    	
+        $punto_cercano=DB::TABLE('puertos')
+                            ->select(DB::RAW("nom_puerto,geometry,st_distance(ST_Transform(ST_SetSRID(ST_GeomFromText('POINT(-71.107 12.028)'),4326),3857),ST_Transform(geometry,3857)) as distancia"))
+                            ->orderby('distancia')
+                            ->limit(1)
+                            ->get();
+
         return view('pages.home', array('arribos_capitanias'=>$arribos_capitanias,'num_naves'=>$num_naves));
     } 
 }
